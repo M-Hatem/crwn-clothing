48f033f5534687f7b215606e3f53578233b5d651
MadoPlus <ma7modmado@gmail.com>
2022-11-18 17:26:10 +0200

    Added Sass package

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-const App = () => (
-  <header className="categories-container">
-    <div className="category-container">
-      {/* <img> */}
-      <div className="category-body-container">
-        <h2>Hats</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      {/* <img> */}
-      <div className="category-body-container">
-        <h2>Jackets</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      {/* <img> */}
-      <div className="category-body-container">
-        <h2>Sneakers</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      {/* <img> */}
-      <div className="category-body-container">
-        <h2>Women</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-    <div className="category-container">
-      {/* <img> */}
-      <div className="category-body-container">
-        <h2>Men</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-  </header>
-);
+import "./categories.styles.scss";
+
+const App = () => {
+  // To intialize categories array instead of hardcoding it
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Women",
+    },
+    {
+      id: 5,
+      title: "Men",
+    },
+  ];
+
+  return (
+    <header className="categories-container">
+      {categories.map(({ id, title }) => {
+        return (
+          <div className="category-container" key={id}>
+            {/* <img> */}
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        );
+      })}
+    </header>
+  );
+};
 
 export default App;
